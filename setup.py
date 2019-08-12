95122ddfc243a2048213068b79117ea1577db197
Kristopher Kyle <kristopherkyle1@gmail.com>
2019-08-12 18:29:42 +0900

    updated webpage

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-your-username",
+    version="0.04",
+    author="Kristopher Kyle",
+    author_email="kristopherkyle1@gmail.com",
+    description="A simple Python toolkit for corpus analyses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kristopherkyle/corpus_toolkit/tree/master/corpus_toolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
